7dfd2d54115e642247942788775d78271a1339a9
Greg <19491678+gmoguel@users.noreply.github.com>
2026-08-18 14:53:44 -0500

    Add asset profile application

---
+++ asset_profile.py
@@ -0,0 +1,16 @@
+hostname  = input("What is the Hostname?\n")
+IP_Address = input("What is the IP Address?\n")
+Operating_System = input("What is the Operating System?\n")
+Asset_Owner = input("Who is the Asset Owner?\n")
+
+print("================================")
+print("        Asset Profile           ")
+print("================================")
+
+print("Hostname:" + hostname)
+print("IP Address:" + IP_Address)
+print("Operating System:" + Operating_System)
+print("Asset Owner:" + Asset_Owner)
+
+print("================================")
+
